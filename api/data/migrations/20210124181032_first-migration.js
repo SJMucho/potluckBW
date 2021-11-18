3e4942d9b96e62caeb30f016346293346e67311b
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-11-17 20:00:11 -0500

    port problem?

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -3,19 +3,44 @@ exports.up = async (knex) => {
     users.increments("user_id");
     users.string("username", 200).notNullable();
     users.string("password", 200).notNullable();
-    users.timestamps(false, true);
+    // users.timestamps(false, true);
   });
 
   await knex.schema.createTable("food_items", (food_items) => {
     food_items.increments("item_id");
-    food_items.string("item_name").notNullable().unique();
+    food_items.string("item_name", 128).notNullable().unique();
+    food_items.string("item_type").notNullable();
   });
 
-  await knex.schema.createTable()
+  await knex.schema.createTable("organizers", (organizers) => {
+    organizers.increments("org_id");
+    organizers.string("location").notNullable();
+    organizers
+      .integer("users_id")
+      .unsigned()
+      .notNullable()
+      .references("users_id")
+      .inTable("users")
+      .onDelete("CASCADE");
+  });
+
+  await knex.schema.createTable("potluck_events", (potluck_events) => {
+    tbl.increments("potluck_id");
+    tbl.date("date").notNullable();
+    tbl.string("location").notNullable();
+    tbl
+      .integer("org_id")
+      .unsigned()
+      .notNullable()
+      .references("org_id")
+      .inTable("organizers")
+      .onDelete("CASCADE");
+  });
 };
 
-
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("potluck_events");
+  await knex.schema.dropTableIfExists("organizers");
   await knex.schema.dropTableIfExists("food_items");
   await knex.schema.dropTableIfExists("users");
 };
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
-
-// const server = require('./api/server')
-
-// const port = process.env.PORT
-
-// server.listen(port, () => {
-//   console.log('listening on ' + port)
-// })
-
 require("dotenv").config();
+
 const server = require("./api/server");
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT;
 
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+server.listen(port, () => {
+  console.log("listening on " + port);
 });
+
+// require("dotenv").config();
+// const server = require("./api/server");
+
+// const PORT = process.env.PORT || 5000;
+
+// server.listen(PORT, () => {
+//   console.log(`\n=== Server listening on port ${PORT} ===\n`);
+// });
